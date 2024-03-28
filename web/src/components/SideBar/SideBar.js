7ad39b0cc169a79f7af89eead2a0ff7bb2543561
Mădălina Sandu <sandu.madalina151@gmail.com>
2024-03-28 19:20:55 +0200

    Added sidebar component

---
+++ web/src/components/SideBar/SideBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Grid} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+import {MenuItems as sidebarItems} from '../MenuItems.js';
+import './SideBar.scss';
+
+function SideBar()
+{
+    const navigate = useNavigate();
+
+    return(
+        <Grid item className='sidebar' sx={{display: {xs: 'none', md: 'none', lg: 'block'}}}>
+            <div className='sidebar-content'>
+                <div className='logo'>DAILY</div>
+                {sidebarItems.map((value, key) => (
+                    <button className='button' id={window.location.pathname.match(value.route)? 'active' : ''} onClick={() => {navigate(value.route)}}>
+                        <span className='icon'>{value.icon}</span>
+                        <span>{value.name}</span>
+                    </button>
+                ))}
+            </div>
+        </Grid>
+    );
+}
+
+export default SideBar;
