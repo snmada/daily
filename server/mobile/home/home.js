e499174ff984112c85c127354b6d10c86204e30f
Mădălina Sandu <sandu.madalina151@gmail.com>
2024-05-31 19:20:31 +0300

    Added functionality to view recommendation

--- server/mobile/home/home.js
+++ server/mobile/home/home.js
@@ -17,5 +17,22 @@ router.get('/user-data/:id', (req, res) => {
         }
     );
 });
+
+router.get('/recommendation/:id', (req, res) => {
+    db.query(
+        `SELECT recommendation FROM medical_records 
+        WHERE uuid_patient = ? AND (deleted_on IS NULL) ORDER BY created_on DESC LIMIT 1`, req.params.id,
+        (error, result) => {
+            if(error)
+            {
+                res.status(500).send();
+            }
+            else
+            {
+                result.length && res.status(200).send(result[0].recommendation);
+            }
+        }
+    );
+});
   
 module.exports = router;
